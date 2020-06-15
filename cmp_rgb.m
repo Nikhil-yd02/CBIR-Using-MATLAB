4b8eb792b76564e6b28ff1b48b3f065aadb5a5e5
NIKHIL YADAV <58444677+Nikhil-yd02@users.noreply.github.com>
2020-06-15 22:26:22 +0530

    Update cmp_rgb.m

--- cmp_rgb.m
+++ cmp_rgb.m
@@ -1,9 +1,13 @@
-%Enter the path of the folder
-folder = 'D:\dataset';
+clear;
+clc;
 
-%read images with specific extention
+%Enter the path of the folder from which you want to search for similar images:
+folder = 'C:\Users\NIKHIL YADAV\Desktop\CBIR\Sample_images';
+
+%read all images with specific extention(e.g.-jpg/jpeg etc):
 filenames = dir(fullfile(folder,'*.jpg'));
 
+%total no. of images present in folder:
 totalImages = numel(filenames);
 
 %reading all test images and storing in array:
@@ -11,6 +15,7 @@ for n =1:totalImages
     %it will specify image name with full path and extention:
     g = fullfile(folder,filenames(n).name);
     
+    %reading and storing dataset images into array:
     images = imread(g);
     Image{n} = images;
 end
@@ -26,14 +31,14 @@ for n = 1:totalImages
     Blue = Orgimg (:,:,3);
 
 
-    %Spliting test images into rgb 
+    %Spliting test images into rgb:
     red = Image{n}(:,:,1);
     green = Image{n}(:,:,2);
     blue = Image{n}(:,:,3);
 
   
 
-    %conversion of image into vector
+    %conversion of image into vector:
     r1 = imhist(Red)./numel(Red);
     r2 = imhist(red)./numel(red);
     
@@ -45,12 +50,13 @@ for n = 1:totalImages
     b1 = imhist(Blue)./numel(Blue);
     b2 = imhist(blue)./numel(blue);
 
-    %calculating the similarity
+    %calculating the similarity using euclidean distance method:
     r = sum(sqrt(r1-r2).^2);
     g = sum(sqrt(g1-g2).^2);
     b = sum(sqrt(b1-b2).^2);
     
-    %Displaying the output
+    %Displaying the output :
+    %Use comparision values according to your requirements:
     if (abs(r) <= 8.0000e-17 ) && (abs(r) >= 0)&&(abs(g) <= 6.0000e-17 ) && (abs(g) >= 0)&&(abs(b) <= 6.0000e-17 ) && (abs(b) >= 0)
         figure;
         imshow(Image{n});
